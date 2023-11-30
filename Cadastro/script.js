443a1ae3f8090d134112294e8b3e68fc983fc30b
luanabrizola <130062674+luanabrizola@users.noreply.github.com>
2023-11-30 11:49:04 -0300

    Java.alert
    Alerta de preencher todos os dados (não esta funcionando quando é para aparecer que esta tudo preenchido, professor ficou de verificar!)

--- Cadastro/script.js
+++ Cadastro/script.js
@@ -12,3 +12,42 @@ function menu(){
         imgMenu.src = "../Imagens/menu.svg"
     }
 }
+
+function validaRegisterForm(){
+    const form = document.getElementById("register-form");
+    // console.log(form.elements['idpassagem'])
+    // const idpasssagem= form.elements['idpassagem'].value;
+    const idpassagem = document.querySelector('#idpassagem').value
+    console.log(idpassagem)
+
+    const duracao=form.elements['duracao'].value;
+    const dataembarque=form.elements['dataembarque'].value;
+    const dataretorno=form.elements['dataretorno'].value;
+    const destino=form.elements['destino'].value;
+    const origem=form.elements['origem'].value;
+    const classe=form.elements['classe'].value;
+
+    
+    
+    if  (idpassagem != '' && duracao != '' ){
+        alert('Dados cadastrados');
+        form.reset();
+        return true;
+    }
+    else{
+        alert('Preencha todos os dados');
+        return false;
+
+    }
+
+    // if  (idpasssagem!='' && duracao && dataembarque && dataretorno && destino && origem && classe){
+    //     alert('Dados cadastrados');
+    //     form.reset();
+    //     return true;
+    // }
+    // else{
+    //     alert('Preencha todos os dados');
+    //     return false;
+
+    // }
+}
