4e8895b36a6914db27ba2b78dce46ca646ed7ade
luanabrizola <130062674+luanabrizola@users.noreply.github.com>
2023-12-02 16:40:33 -0300

    Tentativa de fazer funcionar o botao
    ainda não deu certo!

--- Cadastro/script.js
+++ Cadastro/script.js
@@ -1,53 +1,55 @@
 let menuMobile = document.querySelector('.lista-mobile')
 let imgMenu = document.querySelector('#imgMenu')
 
-function menu(){
+function menu() {
 
-    if(menuMobile.style.left == '-100%'){
+    if (menuMobile.style.left == '-100%') {
         menuMobile.style.left = '0%';
         imgMenu.src = "../Imagens/close.svg"
     }
-    else{
+    else {
         menuMobile.style.left = '-100%'
         imgMenu.src = "../Imagens/menu.svg"
     }
 }
 
-function validaRegisterForm(){
+
+
+
+
+
+
+
+
+function validaRegisterForm() {
     const form = document.getElementById("register-form");
-    // console.log(form.elements['idpassagem'])
-    // const idpasssagem= form.elements['idpassagem'].value;
-    const idpassagem = document.querySelector('#idpassagem').value
-    console.log(idpassagem)
+    const idpassagem = form.elements['idpassagem'].value;
+    const duracao = form.elements['duracao'].value;
+    const dataembarque = form.elements['dataembarque'].value;
+    const dataretorno = form.elements['dataretorno'].value;
+    const destino = form.elements['destino'].value;
+    const origem = form.elements['origem'].value;
+    const classe = form.elements['classe'].value;
 
-    const duracao=form.elements['duracao'].value;
-    const dataembarque=form.elements['dataembarque'].value;
-    const dataretorno=form.elements['dataretorno'].value;
-    const destino=form.elements['destino'].value;
-    const origem=form.elements['origem'].value;
-    const classe=form.elements['classe'].value;
+    console.log(idpassagem, duracao, dataembarque, dataretorno, destino, origem, classe);
 
-    
-    
-    if  (idpassagem != '' && duracao != '' ){
+    if (idpassagem !== '' && duracao !== '' && dataembarque !== '' && dataretorno !== '' && destino !== '' && origem !== '' && classe !== '') {
         alert('Dados cadastrados');
         form.reset();
-        return true;
-    }
-    else{
+    } else {
         alert('Preencha todos os dados');
-        return false;
-
     }
-
-    // if  (idpasssagem!='' && duracao && dataembarque && dataretorno && destino && origem && classe){
-    //     alert('Dados cadastrados');
-    //     form.reset();
-    //     return true;
-    // }
-    // else{
-    //     alert('Preencha todos os dados');
-    //     return false;
-
-    // }
 }
+
+    //if  (idpassagem != '' && duracao != '' ){
+    //    alert('Dados cadastrados');
+    //    form.reset();
+    //  return true;
+    //}
+    //else{
+    //  alert('Preencha todos os dados');
+    //return false;
+
+
+
+
